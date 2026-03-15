054f1d6e7f7116a2f576c2d39a67cf89ce38ccf4
Bryan <bybryan001@gmail.com>
2026-03-14 23:38:54 -0600

    chore: add validate partial movie

--- src/schemas/movies.ts
+++ src/schemas/movies.ts
@@ -45,4 +45,41 @@ export class MovieSchema {
 
         return baseResult;
     }
+
+    async validatePartialMovie(data: unknown){
+        const baseResult = this.baseMovieSchema.partial().safeParse(data)
+
+        if (!baseResult.success) {
+            return baseResult;
+        }
+
+        const allowedGenres = await this.getGenres();
+
+        if (!allowedGenres.length) {
+            throw new Error("getGenres must return at least one genre");
+        }
+
+        if(!baseResult?.data?.genres){
+            return baseResult
+        }
+
+        const invalidGenres =  baseResult.data.genres.filter(
+            (genre) => !allowedGenres.includes(genre)
+        );
+
+        if (invalidGenres.length > 0) {
+            return {
+                success: false as const,
+                error: {
+                    issues: invalidGenres.map((genre) => ({
+                        code: "custom",
+                        path: ["genres"],
+                        message: `Invalid genre: ${genre}`
+                    }))
+                }
+            };
+        }
+
+        return baseResult;
+    }
 }
