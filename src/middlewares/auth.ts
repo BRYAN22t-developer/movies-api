023ff2988fa9c009044f172dfacf53046f20bd87
Bryan <bybryan001@gmail.com>
2026-03-19 14:33:14 -0600

    add auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,27 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken"
+
+const unAuthEndPoints = ["/", "/login"]
+
+export function auth({ req, res, next }: { req: Request, res: Response, next: NextFunction }) {
+    if (endPointIsAllowed(req.path)) return next()
+
+    const authToken = req.cookies?.authToken
+
+    if (!authToken) return res.json({ "Error": "Missing auth token" })
+
+    try {
+        const SECRET_KEY = process.env.SECRET_KEY
+        if (!SECRET_KEY) throw new Error("SECRET_KEY is not defined")
+
+        const payload = jwt.verify(authToken, SECRET_KEY) as Express.UserPayload
+        req.user = payload
+        return next()
+    } catch (e) {
+        return res.status(401).json({ error: "Invalid token" });
+    }
+}
+
+function endPointIsAllowed(path: string) {
+    return unAuthEndPoints.includes(path)
+}
